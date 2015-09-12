384878607de81a74d9e1e2c861889b3e3968d1d0
Chris Boyle <chris@boyle.name>
2015-09-12 13:48:31 +0100

    Initial commit

---
+++ app/src/main/java/name/boyle/chris/tumblrredirector/MainActivity.java
@@ -0,0 +1,28 @@
+package name.boyle.chris.tumblrredirector;
+
+import android.app.*;
+import android.os.*;
+import android.content.*;
+import android.net.*;
+import java.util.regex.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(final Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		Uri uri = getIntent().getData();
+		if (uri == null || uri.getPathSegments().size() < 2 || !"post".equals(uri.getPathSegments().get(0))) {
+			Toast.makeText(this, getString(R.string.no_url), Toast.LENGTH_SHORT).show();
+		} else {
+			final String user = uri.getHost().substring(0, uri.getHost().indexOf('.'));
+			final String postId = uri.getPathSegments().get(1);
+			Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(getString(R.string.tumblr_app_uri), user, postId)));
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(i);
+		}
+		finish();
+	}
+}
